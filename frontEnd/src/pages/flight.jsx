f985b35be1d09efec07474cd125f3bde5b9c7f74
Rakibul-rahi <rakibulislam.rahi.rir@gmail.com>
2025-02-23 16:51:12 +0600

    login,signup, navbar added

---
+++ frontEnd/src/pages/flight.jsx
@@ -0,0 +1,12 @@
+// src/pages/flight.jsx
+import React from "react";
+
+const Flight = () => {
+  return (
+    <div>
+      <h1>Flight Page</h1>
+    </div>
+  );
+};
+
+export default Flight;  // This is the default export
---
+++ frontEnd/src/pages/hotel.jsx
@@ -0,0 +1,12 @@
+// src/pages/hotel.jsx
+import React from "react";
+
+const Hotel = () => {
+  return (
+    <div>
+      <h1>Hotel Page</h1>
+    </div>
+  );
+};
+
+export default Hotel;  // This is the default export
